10631aed803a573d5e3c7544cdc77d7ee82ee417
unknown <keysa.nurzabila2008@smkn4-tsm.sch.id>
2024-10-31 13:22:13 +0700

    first commit

---
+++ app/Models/Biodata.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Biodata extends Model
+{
+    public static $nama ='Hasni Fitriani';
+    public static $kelas ='11 PPLG 3';
+    public static $sekolah ='SMKN 4 Tasikmalaya';
+    public static $alamat = 'Kp.Purbasari';
+    public static $hobi = 'Nari';
+    public static $email = 'hasni.fitriani2009@smkn4-tsm.sch.id';
+    public static $besti = 'faridaaaaa';
+}
+
